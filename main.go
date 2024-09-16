eda543662955325c17e6920cfe9f00b7af45f4d0
TWINNA SANDBOX <twinniation.sandbox@gmail.com>
2024-09-16 08:12:17 +0000

    cheng

--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+    "fmt"
+    "syscall"
+    "unsafe"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+var (
+    user32           = syscall.NewLazyDLL("user32.dll")
+    procMessageBoxW  = user32.NewProc("MessageBoxW")
+)
+
+const (
+    MB_OK = 0x00000000
+)
+
+func MessageBox(hwnd uintptr, caption, title string, flags uint) int {
+    ret, _, _ := procMessageBoxW.Call(
+        hwnd,
+        uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(caption))),
+        uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(title))),
+        uintptr(flags))
+    return int(ret)
 }
 
 func main() {
-	http.HandleFunc("/", helloHandler)
-	fmt.Println("Starting server at port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+    var num1, num2 int
+    fmt.Print("Enter the first number: ")
+    fmt.Scanln(&num1)
+    fmt.Print("Enter the second number: ")
+    fmt.Scanln(&num2)
+    sum := num1 + num2
+    sumStr := fmt.Sprintf("The sum of %d and %d is: %d", num1, num2, sum)
+    MessageBox(0, sumStr, "Sum Result", MB_OK)
 }
